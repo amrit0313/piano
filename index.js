fad4e732737510de15706648aba5f1603d921c76
Amrit Gnawali <amrtz0313@gmail.com>
2023-12-27 17:14:19 +0545

    initial commit

---
+++ index.js
@@ -0,0 +1,332 @@
+var sound = document.getElementById("btn1");
+sound.addEventListener('click',function(){
+    var a= new Audio("c3.mp3");
+    a.play();
+});
+var sound1 = document.getElementById("btn2");
+sound1.addEventListener('click',function(){
+    var a1= new Audio("c-3.mp3");
+    a1.play();
+});
+
+var sound2 = document.getElementById("btn3");
+sound2.addEventListener('click',function(){
+    var a2= new Audio("d3.mp3");
+    a2.play();
+});
+
+
+var sound3 = document.getElementById("btn4");
+sound3.addEventListener('click',function(){
+    var a3= new Audio("d-3.mp3");
+    a3.play();
+});
+
+var sound4 = document.getElementById("btn5");
+sound4.addEventListener('click',function(){
+    var a4= new Audio("e3.mp3");
+    a4.play();
+});
+
+var sound5 = document.getElementById("btn6");
+sound5.addEventListener('click',function(){
+    var a5= new Audio("f3.mp3");
+    a5.play();
+});
+
+var sound6 = document.getElementById("btn7");
+sound6.addEventListener('click',function(){
+    var a6= new Audio("f-3.mp3");
+    a6.play();
+});
+
+var sound7 = document.getElementById("btn8");
+sound7.addEventListener('click',function(){
+    var a7= new Audio("g3.mp3");
+    a7.play();
+});
+
+var sound8 = document.getElementById("btn9");
+sound8.addEventListener('click',function(){
+    var a8= new Audio("g-3.mp3");
+    a8.play();
+});
+
+var sound9 = document.getElementById("btn10");
+sound9.addEventListener('click',function(){
+    var a9= new Audio("a3.mp3");
+    a9.play();
+});
+
+var sound10 = document.getElementById("btn11");
+sound10.addEventListener('click',function(){
+    var a10= new Audio("a-3.mp3");
+    a10.play();
+});
+
+var sound11 = document.getElementById("btn12");
+sound11.addEventListener('click',function(){
+    var a11= new Audio("b3.mp3");
+    a11.play();
+});
+
+var sound12 = document.getElementById("btn13");
+sound12.addEventListener('click',function(){
+    var a12= new Audio("c4.mp3");
+    a12.play();
+});
+
+var sound13 = document.getElementById("btn14");
+sound13.addEventListener('click',function(){
+    var a13= new Audio("c-4.mp3");
+    a13.play();
+});
+
+var sound14 = document.getElementById("btn15");
+sound14.addEventListener('click',function(){
+    var a14= new Audio("d4.mp3");
+    a14.play();
+});
+
+var sound15 = document.getElementById("btn16");
+sound15.addEventListener('click',function(){
+    var a15= new Audio("d-4.mp3");
+    a15.play();
+});
+
+var sound16 = document.getElementById("btn17");
+sound16.addEventListener('click',function(){
+    var a16= new Audio("e4.mp3");
+    a16.play();
+});
+
+var sound17 = document.getElementById("btn18");
+sound17.addEventListener('click',function(){
+    var a17= new Audio("f4.mp3");
+    a17.play();
+});
+
+var sound18 = document.getElementById("btn19");
+sound18.addEventListener('click',function(){
+    var a18= new Audio("f-4.mp3");
+    a18.play();
+});
+
+var sound19 = document.getElementById("btn20");
+sound19.addEventListener('click',function(){
+    var a19= new Audio("g4.mp3");
+    a19.play();
+});
+
+var sound20 = document.getElementById("btn21");
+sound20.addEventListener('click',function(){
+    var a20= new Audio("g-4.mp3");
+    a20.play();
+});
+
+var sound21 = document.getElementById("btn22");
+sound21.addEventListener('click',function(){
+    var a21= new Audio("a4.mp3");
+    a21.play();
+});
+
+var sound22 = document.getElementById("btn23");
+sound22.addEventListener('click',function(){
+    var a22= new Audio("a-4.mp3");
+    a22.play();
+});
+
+var sound23 = document.getElementById("btn24");
+sound23.addEventListener('click',function(){
+    var a23= new Audio("b4.mp3");
+    a23.play();
+}); 
+
+var sound24 = document.getElementById("btn25");
+sound24.addEventListener('click',function(){
+    var a24= new Audio("c5.mp3");
+    a24.play();
+});
+
+var sound25 = document.getElementById("btn26");
+sound25.addEventListener('click',function(){
+    var a25= new Audio("c-5.mp3");
+    a25.play();
+});
+
+var sound26 = document.getElementById("btn27");
+sound26.addEventListener('click',function(){
+    var a26= new Audio("d5.mp3");
+    a26.play();
+});
+
+var sound27 = document.getElementById("btn28");
+sound27.addEventListener('click',function(){
+    var a27= new Audio("d-5.mp3");
+    a27.play();
+});
+
+var sound28 = document.getElementById("btn29");
+sound28.addEventListener('click',function(){
+   var a28= new Audio("e5.mp3");
+   a28.play();
+});
+
+var sound29 = document.getElementById("btn30");
+sound29.addEventListener('click',function(){
+    var a29= new Audio("f5.mp3");
+    a29.play();
+});
+
+var sound30 = document.getElementById("btn31");
+sound30.addEventListener('click',function(){
+    var a30= new Audio("f-5.mp3");
+    a30.play();
+});
+
+var sound31 = document.getElementById("btn32");
+sound31.addEventListener('click',function(){
+    var a31= new Audio("g5.mp3");
+    a31.play();
+}); 
+
+var sound32 = document.getElementById("btn33");
+sound32.addEventListener('click',function(){
+    var a32= new Audio("g-5.mp3");
+    a32.play();
+});
+
+var sound33 = document.getElementById("btn34");
+sound33.addEventListener('click',function(){
+    var a33= new Audio("a5.mp3");
+    a33.play();
+});
+
+var sound34 = document.getElementById("btn35");
+sound34.addEventListener('click',function(){
+    var a34= new Audio("a-5.mp3");
+    a34.play();
+});
+
+var sound35 = document.getElementById("btn36");
+sound35.addEventListener('click',function(){
+    var a35= new Audio("b5.mp3");
+    a35.play();
+});
+
+var sound36 = document.getElementById("btn37");
+sound36.addEventListener('click',function(){
+    var a36= new Audio("c6.mp3");
+    a36.play();
+});
+
+
+var sound37 = document.getElementById("btn38");
+sound37.addEventListener('click',function(){
+    var a37= new Audio("c-6.mp3");
+    a37.play();
+});
+
+var sound38 = document.getElementById("btn39");
+sound38.addEventListener('click',function(){
+    var a38= new Audio("d6.mp3");
+    a38.play();
+});
+
+var sound39 = document.getElementById("btn40");
+sound39.addEventListener('click',function(){
+    var a39= new Audio("d-6.mp3");
+    a39.play();
+});
+
+var sound40 = document.getElementById("btn41");
+sound40.addEventListener('click',function(){
+    var a40= new Audio("e6.mp3");
+    a40.play();
+});
+
+var sound41 = document.getElementById("btn42");
+sound41.addEventListener('click',function(){
+    var a41= new Audio("f6.mp3");
+    a41.play();
+});
+
+var sound42 = document.getElementById("btn43");
+sound42.addEventListener('click',function(){
+    var a42= new Audio("f-6.mp3");
+    a42.play();
+});
+
+var sound43 = document.getElementById("btn44");
+sound43.addEventListener('click',function(){
+    var a43= new Audio("g6.mp3");
+    a43.play();
+});
+
+var sound44 = document.getElementById("btn45");
+sound44.addEventListener('click',function(){
+    var a44= new Audio("g-6.mp3");
+    a44.play();
+});
+
+var sound45 = document.getElementById("btn46");
+sound45.addEventListener('click',function(){
+    var a45= new Audio("a6.mp3");
+    a45.play();
+});
+
+var sound46 = document.getElementById("btn47");
+sound46.addEventListener('click',function(){
+    var a46= new Audio("a-6.mp3");
+    a46.play();
+});
+
+var sound47 = document.getElementById("btn48");
+sound47.addEventListener('click',function(){
+    var a47= new Audio("b6.mp3");
+    a47.play();
+});
+
+var sound48 = document.getElementById("btn49");
+sound48.addEventListener('click',function(){
+    var a48= new Audio("c7.mp3");
+    a48.play();
+});
+
+
+// create a mapping between keys and audio files
+var keyToAudio = {
+    'a': {button: 'btn1', audio: new Audio('a3.mp3')},
+    'b': {button: 'btn2', audio: new Audio('b3.mp3')},
+    'c': {button: 'btn3', audio: new Audio('c3.mp3') },
+    'd': {button: 'btn4', audio: new Audio('d3.mp3') },
+    'e': {button: 'btn5', audio: new Audio('e3.mp3') },
+    'f': {button: 'btn6', audio: new Audio('f3.mp3') },
+    'g': {button: 'btn7', audio: new Audio('g3.mp3') }
+  };
+  
+  document.addEventListener('keydown', function(event) {
+    var key = event.key;
+    var mapping = keyToAudio[key];
+  
+    if (mapping) {
+      // get the button
+      var button = document.getElementById(mapping.button);
+  
+      // create a new 'click' event
+      var clickEvent = new MouseEvent('click', {
+        bubbles: true,
+        cancelable: true,
+        view: window
+      });
+  
+      // dispatch the event
+      button.dispatchEvent(clickEvent);
+  
+      // play the audio
+      mapping.audio.play();
+    }
+  });
+
+
+
